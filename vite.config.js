805fd7454f41506cdf7407f4fceb855e0fb7e9b1
민병욱 <byungwook-min@naver.com>
2025-01-26 12:52:51 +0900

    feat: initialize Vite with React and set up project structure for Orbit-Web
    - Initialize a new Vite project with React template
    - Configure project directory structure with components and pages
    
    YAF-93

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './public/assets'),
+    },
+  },
+});
